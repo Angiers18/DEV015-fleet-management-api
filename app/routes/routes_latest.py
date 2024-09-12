be2742ad9fd191103318e22639781f2dd1177c96
Angie <angiesamper18@gmail.com>
2024-09-11 22:08:30 -0300

    fix: fixed path errors

--- app/routes/routes_latest.py
+++ app/routes/routes_latest.py
@@ -2,9 +2,9 @@ from flask import Blueprint
 
 
 
-bp_route_latest = Blueprint('bp_route_latest', __name__, url_prefix='/latest')
+bp_route_latest = Blueprint('bp_route_latest', __name__)
 
-@bp_route_latest.route('/', methods=['GET'])
+@bp_route_latest.route('/latest', methods=['GET'])
 def get_lastest():
     return 'Holiis'
 
