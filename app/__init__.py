c79a45a46c75a10af5bc424cde508f3df248653e
Angie <angiesamper18@gmail.com>
2024-09-12 23:05:28 -0300

    feat: returns latest trajectories

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from app.database.db import db
 from app.routes.routes_taxis import bp_route_taxis
 from app.routes.routes_home import bp_route_home
 from app.routes.routes_trajectories import bp_route_trajectories
-from app.routes.routes_latest import bp_route_latest
+from .routes.routes_latest import bp_route_latest
+
 
 
 def create_app() :
@@ -18,12 +19,11 @@ def create_app() :
 
     app.register_blueprint(bp_route_taxis)
 
-    bp_route_trajectories.register_blueprint(bp_route_latest)
     app.register_blueprint(bp_route_trajectories)
-    
+
+    app.register_blueprint(bp_route_latest)
 
     # Registra los modelos
     with app.app_context() :
         db.create_all()          # Crea las tablas en la base de datos
         return app
-
--- app/routes/routes_latest.py
+++ app/routes/routes_latest.py
@@ -1,10 +1,22 @@
-from flask import Blueprint
-
-
+from sqlalchemy import func
+from flask import Blueprint, jsonify
+from app.database.db import db
+from app.models import Trajectory
 
 bp_route_latest = Blueprint('bp_route_latest', __name__)
 
-@bp_route_latest.route('/latest', methods=['GET'])
-def get_lastest():
-    return 'Holiis'
+@bp_route_latest.route('/trajectories/latest', methods=['GET'])
+def get_latest_trajectories():
 
+         # Subconsulta que obtiene la última trayectoria por taxi_id
+    subquery = db.session.query(
+        Trajectory.taxi_id,
+        func.max(Trajectory.date).label('latest_trajectories')
+    ).group_by(Trajectory.taxi_id).subquery()
+
+    latest = db.session.query(subquery).all()
+    print('ultimas rutas y taxi_id', latest)
+
+    latest_data = [{'taxi_id': row[0], 'latest_trajectories': row[1]} for row in latest]
+
+    return jsonify(latest_data)
