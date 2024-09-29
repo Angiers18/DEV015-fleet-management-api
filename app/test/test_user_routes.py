48072b6a788d24927d6917c57d5a00bf55940b19
Angie <angiesamper18@gmail.com>
2024-09-29 20:09:01 -0300

    test: mock the app response get users with limit

--- app/test/test_user_routes.py
+++ app/test/test_user_routes.py
@@ -25,3 +25,18 @@ def test_create_user(test_app, client):
     # retorna el ID del usuario creado
     print('Nuevo ID creado', response_data['id'])
     return response_data['id']
+
+ 
+
+def test_get_users_with_limit(test_app, client):
+
+
+    params = {'page': 2, 'limit': 5 }
+    response = client.get('/users', query_string=params)
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert len(data) == 5
+
